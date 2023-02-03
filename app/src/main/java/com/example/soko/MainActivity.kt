5f96c3614ab14ab37f9b9f33eb6a4d65a0a88e2e
Lilytreasure <lilyngure@gmail.com>
2023-02-03 16:09:07 +0300

    Dependencies set

---
+++ app/src/main/java/com/example/soko/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.soko
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    //fetch and catch data from fakestore api
+
+
+
+
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+
+    }
+}
